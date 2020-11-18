ff4233ffa444459706d566a4f0861bf1bd1bed5f
Yuliana Alvarado <yalvarado924@gmail.com>
2020-11-18 11:43:05 -0600

    commit tree ece70157e6f5bf567bbf300ec0ad256bf15bc4bd

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
 # code here!
+class School
+
+    attr_accessor :name, :roster
+    
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        roster[grade] = [] if @roster[grade] == nil
+        roster[grade] << name
+    end
+
+    def grade(number)
+        @roster[number]
+    end
+
+    def sort
+        sorted = {}
+        @roster.each do |grade, name|
+        sorted[grade] = name.sort
+        end
+        sorted
+    end
+end
